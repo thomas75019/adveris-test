843356bb9b7854af29913d5d66404c7d87edb154
Thomas75019 <tlarousse3@gmail.com>
2024-12-31 16:58:06 +0100

    Add lang files, and restore some files that disappeared

---
+++ resources/lang/en/articles.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'list' => 'Articles List',
+    'new' => 'Create articles',
+    'login' => 'Login',
+    'register' => 'Register',
+    'logout' => 'logout',
+    'title' => 'Title:',
+    'content' => 'Content:',
+    'submit' => 'Submit',
+    'update' => 'Update',
+    'edit' => 'Edit Article'
+];
